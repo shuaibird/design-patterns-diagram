079b1d6a09a60f24ecaa130a56a4c0b6184ab517
shuaibird <shuaibird@icloud.com>
2022-04-21 19:29:19 +0800

    feat: creational - factory

---
+++ creational/factory/abstract-factory.ts
@@ -0,0 +1,33 @@
+interface Component1 {}
+class Component1A implements Component1 {}
+class Component1B implements Component1 {}
+interface Component2 {}
+class Component2A implements Component2 {}
+class Component2B implements Component2 {}
+
+abstract class FactoryBase {
+  public assemble(): void {
+    const comp1 = this.createComponent1()
+    const comp2 = this.createComponent2()
+  };
+  protected abstract createComponent1(): Component1;
+  protected abstract createComponent2(): Component2;
+}
+
+class FactoryA extends FactoryBase {
+  protected createComponent1(): Component1 {
+    return new Component1A();
+  }
+  protected createComponent2(): Component2 {
+    return new Component2A();
+  }
+}
+
+class FactoryB extends FactoryBase {
+  protected createComponent1(): Component1 {
+    return new Component1B();
+  }
+  protected createComponent2(): Component2 {
+    return new Component2B();
+  }
+}
---
+++ creational/factory/factory-method.ts
@@ -0,0 +1,19 @@
+interface Product {}
+class ProductA implements Product {}
+class ProductB implements Product {}
+
+abstract class FactoryBase {
+  public abstract createProduct(): Product;
+}
+
+class FactoryA extends FactoryBase {
+  public createProduct(): Product {
+    return new ProductA()
+  }
+}
+
+class FactoryB extends FactoryBase {
+  public createProduct(): Product {
+    return new ProductB()
+  }
+}
