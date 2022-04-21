9bf629915a21cda1912d2534308779a363cd8976
shuaibird <shuaibird@icloud.com>
2022-04-21 22:42:32 +0800

    feat: creational - singleton

---
+++ creational/singleton.ts
@@ -0,0 +1,14 @@
+class Singleton {
+  private static instance: Singleton
+  private constructor() {}
+
+  public static getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton()
+    }
+    return Singleton.instance
+  }
+}
+
+new Singleton() //impossible
+Singleton.getInstance() == Singleton.getInstance() //true
