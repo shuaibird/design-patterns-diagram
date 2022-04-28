f6466715b688c4431e027089653e480a3af1f952
shuaibird <shuaibird@icloud.com>
2022-04-28 22:12:41 +0800

    feat: structural - facade

---
+++ structural/facade.ts
@@ -0,0 +1,16 @@
+class ComponentA {
+  public static process() {}
+}
+
+class ComponentB {
+  public static process() {}
+}
+
+class Facade {
+  public static process() {
+    ComponentA.process()
+    ComponentB.process()
+  }
+}
+
+Facade.process()
